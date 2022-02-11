79c0f3c1dd076a6e9640054d3a5f2f9f7a3172d7
Terry <70032002+vanceter@users.noreply.github.com>
2022-02-11 11:38:58 -0800

    Update merge_excel_sheets.py
    added excel formatting, changed to used xlsxwriter at least for Ops sheet for now

--- merge_excel_sheets.py
+++ merge_excel_sheets.py
@@ -1,8 +1,7 @@
 # 2022/02/10 Terry Vance
 # importing the module
-import pandas
-import XlsxWriter
-
+import pandas as pd
+import xlsxwriter
 
 # NOTE each of the underlying xls files needs to have the PSLC value - in some of them, the column header needs to be renamed from PS Loc
 # Also need to make sure you export OpsTracker files with the file name option checked
@@ -16,11 +15,11 @@ import XlsxWriter
 
 # reading only the columns needed from each file
 # documentation on pandas read_excel https://pandas.pydata.org/docs/reference/api/pandas.read_excel.html
-f_sites = pandas.read_excel("opstracker_sites.xlsx",usecols=['SITE_NAME','ADDRESS','CITY','COUNTY','PSLC','POWER_METER', 'GEN_STATUS','GEN_PORTABLE_PLUG', 'GEN_PORTABLE_PLUG_TYPE', 'IS_HUB','IS_HUB_MICROWAVE','REMOTE_MONITORING','SITETECH_NAME','SITETECH_MANAGER_NAME'])
-f_gens = pandas.read_excel("opstracker_generators.xlsx", usecols=['PSLC', 'FUEL_TYPE1'])
-f_cells = pandas.read_excel("NorCal_CellInfo.xlsx", usecols=['PSLC', 'eNodeB'])
-f_cells5g = pandas.read_excel("norcal_cell_info_5g.xlsx", usecols=['PSLC', 'GNODEB'])
-f_pge = pandas.read_excel("PSPS_FIRE_TIER.xlsx", usecols=['PSLC', 'Fire Tier', 'PSPS PROB', 'PG&E Fee Property'])
+f_sites = pd.read_excel("opstracker_sites.xlsx",usecols=['SITE_NAME','ADDRESS','CITY','COUNTY','PSLC','POWER_METER', 'GEN_STATUS','GEN_PORTABLE_PLUG', 'GEN_PORTABLE_PLUG_TYPE', 'IS_HUB','IS_HUB_MICROWAVE','REMOTE_MONITORING','SITETECH_NAME','SITETECH_MANAGER_NAME', 'POWER_COMPANY'])
+f_gens = pd.read_excel("opstracker_generators.xlsx", usecols=['PSLC', 'FUEL_TYPE1'])
+f_cells = pd.read_excel("NorCal_CellInfo.xlsx", usecols=['PSLC', 'eNodeB'])
+f_cells5g = pd.read_excel("norcal_cell_info_5g.xlsx", usecols=['PSLC', 'GNODEB'])
+f_pge = pd.read_excel("PSPS_FIRE_TIER.xlsx", usecols=['PSLC', 'Fire Tier', 'PSPS PROB', 'PG&E Fee Property'])
   
 # merging the files using PSLC as the index. There are some duplicates in gen and sites files, lots of duplicates in the cell info because of B2B and 5G gNodeBs
 # documentation on pandas merge https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.merge.html?highlight=merge#pandas.DataFrame.merge
@@ -35,5 +34,37 @@ f_merged_ops = f_merged_ops.merge(f_pge, left_on="PSLC", right_on="PSLC", how="l
   
 # creating 2 new files, the PSPS_Main for Gennie, and a version of it with eNB/gNB for SP
 # documentation on pandas to_excel https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_excel.html?highlight=to_excel
-f_merged.to_excel("PSPS_MAIN_SP.xlsx", index = False, sheet_name='PSPS_MAIN_SP', columns=['POWER_METER','Fire Tier', 'PSPS PROB','PSLC', 'PG&E Fee Property', 'SITE_NAME', 'ADDRESS','CITY','COUNTY', 'GEN_STATUS','FUEL_TYPE1', 'GEN_PORTABLE_PLUG', 'GEN_PORTABLE_PLUG_TYPE', 'REMOTE_MONITORING', 'IS_HUB_MICROWAVE', 'IS_HUB','SITETECH_NAME','SITETECH_MANAGER_NAME', 'eNodeB', 'GNODEB'])
-f_merged_ops.to_excel("PSPS_MAIN.xlsx", index = False, sheet_name='PSPS_MAIN', columns=['POWER_METER','Fire Tier', 'PSPS PROB','PSLC', 'PG&E Fee Property', 'SITE_NAME', 'ADDRESS','CITY','COUNTY', 'GEN_STATUS','FUEL_TYPE1', 'GEN_PORTABLE_PLUG', 'GEN_PORTABLE_PLUG_TYPE', 'REMOTE_MONITORING', 'IS_HUB_MICROWAVE', 'IS_HUB','SITETECH_NAME','SITETECH_MANAGER_NAME'])
+f_merged.to_excel("PSPS_MAIN_SP.xlsx", index = False, sheet_name='PSPS_MAIN_SP', columns=['POWER_METER','Fire Tier', 'PSPS PROB','PSLC', 'PG&E Fee Property', 'SITE_NAME', 'ADDRESS','CITY','COUNTY', 'GEN_STATUS','FUEL_TYPE1', 'GEN_PORTABLE_PLUG', 'GEN_PORTABLE_PLUG_TYPE', 'REMOTE_MONITORING', 'IS_HUB_MICROWAVE', 'IS_HUB','SITETECH_NAME','SITETECH_MANAGER_NAME', 'POWER_COMPANY', 'eNodeB', 'GNODEB'])
+#f_merged_ops.to_excel("PSPS_MAIN.xlsx", index = False, sheet_name='PSPS_MAIN', columns=['POWER_METER','Fire Tier', 'PSPS PROB','PSLC', 'PG&E Fee Property', 'SITE_NAME', 'ADDRESS','CITY','COUNTY', 'GEN_STATUS','FUEL_TYPE1', 'GEN_PORTABLE_PLUG', 'GEN_PORTABLE_PLUG_TYPE', 'REMOTE_MONITORING', 'IS_HUB_MICROWAVE', 'IS_HUB','SITETECH_NAME','SITETECH_MANAGER_NAME'])
+# Change the write tool to include ability to add formatting to the output 
+
+# Format the PSPS_MAIN sheet for Ops
+# establish the xlsxwriter functionality, defining "writer" as the variable for the workbook filename
+writer = pd.ExcelWriter('PSPS_MAIN.xlsx', engine='xlsxwriter')
+# Create the merged sheet and output to the file name based on the writer variable
+f_merged_ops.to_excel(writer, index=False, sheet_name='PSPS_MAIN',columns=['POWER_METER','Fire Tier', 'PSPS PROB','PSLC', 'PG&E Fee Property', 'SITE_NAME', 'ADDRESS','CITY','COUNTY', 'GEN_STATUS','FUEL_TYPE1', 'GEN_PORTABLE_PLUG', 'GEN_PORTABLE_PLUG_TYPE', 'REMOTE_MONITORING', 'IS_HUB_MICROWAVE', 'IS_HUB','SITETECH_NAME','SITETECH_MANAGER_NAME', 'POWER_COMPANY'])
+# Establish the workbook variable
+workbook = writer.book
+
+# Setup some formating definitions
+# formatting for any cells/columns that need to be center justified
+cell_format_center = workbook.add_format()
+cell_format_center.set_align('center')
+
+# Define the worksheet variable
+worksheet = writer.sheets['PSPS_MAIN']
+# Apply some formatting to groups of columns, including cell width and applying the cell formatting previously defined as appropriate
+worksheet.set_column('A:A', 20, cell_format_center)
+worksheet.set_column('B:D', 10, cell_format_center)
+worksheet.set_column('E:E', 18, cell_format_center)
+worksheet.set_column('F:G', 44)
+worksheet.set_column('H:I', 22)
+worksheet.set_column('J:P', 22, cell_format_center)
+worksheet.set_column('Q:Q', 22)
+worksheet.set_column('R:R', 28)
+worksheet.set_column('S:S', 28, cell_format_center)
+# Set some worksheet formatting, including creating filter dropdowns and freeze the top row
+worksheet.freeze_panes(1, 0)
+worksheet.autofilter('A1:R9999')
+# Save the sheet
+writer.save()
